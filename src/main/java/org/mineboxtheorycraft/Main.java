686d16a9d09268ce14815a9df03407c35048969a
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2024-05-25 21:18:27 +0200

    Create bot connection

--- src/main/java/org/mineboxtheorycraft/Main.java
+++ src/main/java/org/mineboxtheorycraft/Main.java
@@ -1,7 +1,15 @@
 package org.mineboxtheorycraft;
 
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.intent.Intent;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DiscordApi api = new DiscordApiBuilder()
+                .setToken(args[0])
+                .addIntents(Intent.MESSAGE_CONTENT)
+                .login().join();
+
     }
 }
