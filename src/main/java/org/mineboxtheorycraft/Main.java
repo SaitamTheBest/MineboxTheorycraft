8386aada0904f38b44e34e1c457606394ff635cd
Matias DEVEZE <matiasdeveze@gmail.com>
2024-06-15 22:41:00 +0200

    change the activity of the bot

--- src/main/java/org/mineboxtheorycraft/Main.java
+++ src/main/java/org/mineboxtheorycraft/Main.java
@@ -2,6 +2,8 @@ package org.mineboxtheorycraft;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.activity.ActivityFlag;
+import org.javacord.api.entity.activity.ActivityType;
 import org.javacord.api.interaction.*;
 import org.mineboxtheorycraft.filedata.FileIOItemData;
 import org.mineboxtheorycraft.filedata.FileIOMessageCommerce;
@@ -10,6 +12,10 @@ import org.mineboxtheorycraft.listener.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -18,7 +24,7 @@ public class Main {
                 .setAllIntents()
                 .login().join();
 
-        //Slash Commands
+        /// ***** Slash Commands *****
         System.out.println("[Start] Enregistrement des SlashCommands");
         SlashCommand.with("add-item","Ajoute un item",Arrays.asList(
             SlashCommandOption.create
@@ -47,37 +53,51 @@ public class Main {
         SlashCommand.with("list-items","Afficher la liste des items suivant un tri").createGlobal(api).join();
         System.out.println("[End] Enregistrement des SlashCommands");
 
-        //SlashCommandsListener
+        /// ***** SlashCommandsListener *****
+
+        // Command /add-item
         api.addSlashCommandCreateListener(new AddItemListener());
 
+        // Command /search
         SearchItemListener searchItemListener = new SearchItemListener();
         api.addSlashCommandCreateListener(searchItemListener);
         api.addSelectMenuChooseListener(searchItemListener);
 
+        // Command /modify
         ModifyItemListener modifyItemListener = new ModifyItemListener();
         api.addSlashCommandCreateListener(modifyItemListener);
         api.addModalSubmitListener(modifyItemListener);
         api.addSlashCommandCreateListener(new MessageCommerceListener());
 
+        // Command /message-commerce
         ModifyMessageCommerceListener modifyMessageCommerceListener = new ModifyMessageCommerceListener();
         api.addButtonClickListener(modifyMessageCommerceListener);
         api.addModalSubmitListener(modifyMessageCommerceListener);
 
+        // Command /craft-item
         CraftItemListener craftItemListener = new CraftItemListener();
         api.addButtonClickListener(craftItemListener);
         api.addSlashCommandCreateListener(craftItemListener);
         api.addSelectMenuChooseListener(craftItemListener);
         api.addModalSubmitListener(craftItemListener);
 
+        // Command /list-items
         ListItemsListener listItemsListener = new ListItemsListener();
         api.addSlashCommandCreateListener(listItemsListener);
         api.addSelectMenuChooseListener(listItemsListener);
 
-
-        //Load items
+        /// ***** Load items *****
         System.out.println("[Start] Lecture des données");
         FileIOItemData.readObjects();
         FileIOMessageCommerce.readMessageInFile();
         System.out.println("[End] Lecture des données");
+
+        /// ***** Activity *****
+        Random rand = new Random();
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(() -> {
+            String nameRandomItem = FileIOItemData.itemArrayList.get(rand.nextInt(FileIOItemData.itemArrayList.size())).getName();
+            api.updateActivity(ActivityType.WATCHING, nameRandomItem);
+            }, 0, 5, TimeUnit.MINUTES);
     }
 }
